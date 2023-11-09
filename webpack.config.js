e04a6628186438fa46b06da3aa8f4ab43bf876ae
Sahil Negi <sahilnegi2008009@gmail.com>
2023-11-09 12:40:08 +0530

    commited

---
+++ webpack.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    // ... other webpack configuration options ...
+  
+    resolve: {
+      modules: ['node_modules'],
+    },
+  
+    // ... other webpack configuration options ...
+  };
+  
